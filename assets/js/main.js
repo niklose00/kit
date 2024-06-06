ddae491e893e86a00193154d94a6d072f79f44d5
Nico <klosenico00@gmail.com>
2024-06-06 11:04:48 +0200

    testing config query

--- assets/js/main.js
+++ assets/js/main.js
@@ -772,8 +772,8 @@ class Regex {
 // Initialisierung mit Konfigurationsladung
 document.addEventListener("DOMContentLoaded", async () => {
   // Konfigurationsdatei laden
-  const config = await ApiService.loadConfiguration();
   console.log("config");
+  const config = await ApiService.loadConfiguration();
   console.log(config);
 
 
